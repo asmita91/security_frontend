5a1c81910604e467676395378371195b78ba6540
asm <94774188+asmita91@users.noreply.github.com>
2024-08-25 03:51:33 +0545

    Update AddProduct.jsx

--- src/components/Admin/AddProduct.jsx
+++ src/components/Admin/AddProduct.jsx
@@ -8,7 +8,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import DOMPurify from "dompurify";
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import sound from "../../assets/sound.wav";
